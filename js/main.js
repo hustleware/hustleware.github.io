3c7dcd9776a9777a69af4ccc1333bf458b8cc452
John Monte <ph.johnmonte@yahoo.com>
2021-01-04 01:13:41 +0800

    202101040113
    added quotes

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 var quotes = [
 	"One day or day one. You decide.",
 	"Hustle in silence. Let your success make the noise.",
-	"All things are difficult before they are easy.",
+	"Have patience. All things are difficult before they are easy.",
 	"Hustle until your haters ask if you're hiring.",
 	"Postpone procrastination.",
 	"Hustle until your idols become you're rivals.",
@@ -10,18 +10,19 @@ var quotes = [
 	"Let your fear of regret be stronger than your fear of anything else.",
 	"Hustle until your bank account looks like a phone number.",
 	"You can't have a million-dollar dream with a minimum-wage work ethic.",
-	"Hustle until you don't have to introduce yourself.",
+	"Hustle until your name no longer needs an introduction.",
 	"You are what you do, not what you say you'll do.",
 	"Hustle until it pains because being broke as fuck hurts even more.",
 	"Never look down on anyone unless you are admiring their shoes.",
 	"Good things may come to those who wait, but only the ones left by those who hustle.",
 	"Don't do the same shit and expect different results.",
 	"You don't have to be great to embrace the hustle, but you have to embrace the hustle to be great.",
-	"It's not about how much sleep you get. It's about what you do when you're awake."
+	"It's not about how much sleep you get. It's about what you do when you're awake.",
+	"The past is in your head, but the future is in your hands."
 ];
 
 function genQuote() {
-	var randNum = Math.floor(Math.random() * 18) + 1;	
+	var randNum = Math.floor(Math.random() * 20) + 1;	
 	document.getElementById('quote').innerHTML = quotes[randNum];
 }
 
