b28a39f20435114ce1009c4291cf97ab105450aa
John Monte <ph.johnmonte@yahoo.com>
2021-01-16 19:39:28 +0800

    202101161939
    lotta design changes

--- js/main.js
+++ js/main.js
@@ -5,26 +5,28 @@ var quotes = [
 	"Hustle until your haters ask if you're hiring.",
 	"Postpone procrastination.",
 	"Hustle until your idols become you're rivals.",
-	"Do something instead of killing time. Because time is killing you.",
+	"Quotes won't work unless you do.",
 	"Hustle until your signature becomes an autograph.",
 	"Let your fear of regret be stronger than your fear of anything else.",
 	"Hustle until your bank account looks like a phone number.",
 	"You can't have a million-dollar dream with a minimum-wage work ethic.",
 	"Hustle until your name no longer needs an introduction.",
-	"You are what you do, not what you say you'll do.",
+	"Discipline is choosing what you want now and what you want most.",	
 	"Hustle until it pains because being broke as fuck hurts even more.",
 	"Never look down on anyone unless you are admiring their shoes.",
 	"Good things may come to those who wait, but only the ones left by those who hustle.",
 	"Don't do the same shit and expect different results.",
 	"You don't have to be great to embrace the hustle, but you have to embrace the hustle to be great.",
-	"It's not about how much sleep you get. It's about what you do when you're awake.",
+	"Don't count the days. Make the days count.",
 	"The past is in your head, but the future is in your hands.",
 	"The more you slack, the less you stack.",
-	"Don't dream about success. Work for it.",	
+	"Don't dream about success. Work for it.",
+	"No more excuses. Yesterday you said tomorrow. Today you'll say done.",
+	"You've got everything it takes, but it will take everything you've got.",
 ];
 
 function genQuote() {
-	var randNum = Math.floor(Math.random() * 19) + 1;	
+	var randNum = Math.floor(Math.random() * 24) + 1;	
 	document.getElementById('quote').innerHTML = quotes[randNum];
 }
 
