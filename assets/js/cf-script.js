c98de904de7f08ed2478bfdff3041554b90dca9d
John Monte <ph.johnmonte@yahoo.com>
2023-10-23 17:30:28 +0800

    202310231730

--- assets/js/cf-script.js
+++ assets/js/cf-script.js
@@ -4,14 +4,9 @@ window.onload = function () {
     context: document.getElementById("cf-context"),
     theme: 'dark',
     showProgressBar: true,
-    // submitCallback: function () {
-    //   conversationalForm.addRobotChatResponse(
-    //     "That is the end of the form. Thank you."
-    //   );
-    // }
   });
 };
 
 let searchParams = new URLSearchParams(window.location.search)
-let param = searchParams.get('subject')
-document.getElementById("subject").value = param;
+let param = searchParams.get('ref')
+document.getElementById("ref").value = param;
